404317d14e940ca081b646c3c2683f4b8a2ebc91
Anesu Cain Mugomba <anesucain@gmail.com>
2023-06-16 11:30:25 +0200

    test if laravel can access env

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/gestalt', function (Request $request) {
     return response('All good!', 200);
 });
+
+Route::get('/environments', function (Request $request) {
+    return response(array('APP_NAME' => env('APP_NAME'), 'APP_ENV' => env('APP_ENV'), 'AWS_LOCATION_SERVICE_KEY' => env('AWS_LOCATION_SERVICE_KEY')), 200);
+});
