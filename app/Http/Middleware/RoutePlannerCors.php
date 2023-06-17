2bfc561835f681ef126946ae6e5a2773d8257a93
Anesu Cain Mugomba <anesucain@gmail.com>
2023-06-17 07:42:04 +0200

    fixed origin url in route planner middleware

--- app/Http/Middleware/RoutePlannerCors.php
+++ app/Http/Middleware/RoutePlannerCors.php
@@ -19,7 +19,7 @@ class RoutePlannerCors
         if (str_contains($request->getSchemeAndHttpHost(), 'localhost')) {
             $origin = 'http://localhost:3000';
         } else {
-            $origin = 'http://portfolio-api.us-east-1.elasticbeanstalk.com';
+            $origin = 'http://anesucain-route-planner.s3-website-us-east-1.amazonaws.com/';
         }
 
         return $next($request)
